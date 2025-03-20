54782acfc0a35b815c691e1f0d6a363be2c95ff7
matthew laidlaw <matthew.james.laidlaw@gmail.com>
2025-03-19 23:29:37 -0700

    cleanup

--- src/Tensor/Utilities/Shape.hpp
+++ src/Tensor/Utilities/Shape.hpp
@@ -9,16 +9,9 @@ auto GetSize(std::array<size_t, Order> const& shape) -> size_t
 }
 
 template <size_t Order>
-auto GetStrides(std::array<size_t, Order> const& shape) -> std::array<size_t, Order>
+auto GetStrides(const std::array<size_t, Order>& shape) -> std::array<size_t, Order>
 {
     std::array<size_t, Order> strides{};
-    if constexpr (Order > 0)
-    {
-        strides[Order - 1] = 1;
-        for (size_t i = Order - 1; i-- > 0;)
-        {
-            strides[i] = strides[i + 1] * shape[i + 1];
-        }
-    }
+    std::exclusive_scan(shape.rbegin(), shape.rend(), strides.rbegin(), static_cast<size_t>(1), std::multiplies<>());
     return strides;
 }
