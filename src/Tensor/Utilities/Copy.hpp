482b4459169e83d5a45fb126f665e4f99e2b2f48
matthew laidlaw <matthew.james.laidlaw@gmail.com>
2025-03-20 09:48:55 -0700

    comment

--- src/Tensor/Utilities/Copy.hpp
+++ src/Tensor/Utilities/Copy.hpp
@@ -1,28 +1,35 @@
 #pragma once
 
+#include <Expect.hpp>
+
 #include <array>
 
-template <typename T1, typename T2, size_t Order, size_t Dim = 0>
-auto CopyElementwiseRecursive(const T1& src, T2& dst, std::array<size_t, Order>& indices) -> void
-{
-    if constexpr (Dim == Order)
-    {
-        dst(indices) = static_cast<T2>(src(indices));
-    }
-    else
-    {
-        for (size_t i = 0; i < src.shape[Dim]; ++i)
-        {
-            indices[Dim] = i;
-            CopyElementwiseRecursive<T1, T2, Order, Dim + 1>(src, dst, indices);
-        }
-    }
-}
-
+/*
+ * Copy the contents of the source container into the destination container, element-by-element.
+ * For use when one of the containers is non-contiguous, and memory can't be copied directly.
+ * Recursively visits ever permutation of indices allowable for the given shape.
+ */
 template <typename T1, typename T2, size_t Order>
-auto CopyElementwise(const T1& src, T2& dst) -> void
+void CopyElementwise(const T1& source, T2& destination)
 {
-    Expect(src.Shape() == dst.Shape());
-    auto indices = std::array<size_t, Order>{0};
-    CopyElementwiseRecursive(src, dst, indices);
+    Expect(source.Shape() == destination.Shape());
+    std::array<size_t, Order> currentIndices{};
+
+    auto recursiveCopy = [&](auto&& recursiveCopy, size_t currentDimension) -> void
+    {
+        if (currentDimension == Order) // base case
+        {
+            destination(currentIndices) = static_cast<typename T2::ValueType>(source(currentIndices));
+        }
+        else // recursive case
+        {
+            for (size_t i = 0; i < source.Shape()[currentDimension]; ++i)
+            {
+                currentIndices[currentDimension] = i;
+                recursiveCopy(recursiveCopy, currentDimension + 1);
+            }
+        }
+    };
+
+    recursiveCopy(recursiveCopy, 0);
 }
--- src/Tensor/Utilities/Shape.hpp
+++ src/Tensor/Utilities/Shape.hpp
@@ -2,12 +2,14 @@
 
 #include <array>
 
+// Calculate the total size of the buffer required to hold the given shape.
 template <size_t Order>
 auto GetSize(std::array<size_t, Order> const& shape) -> size_t
 {
     return std::accumulate(shape.begin(), shape.end(), static_cast<size_t>(1), std::multiplies<>());
 }
 
+// Calculate the strides needed to index into a buffer with the given shape.
 template <size_t Order>
 auto GetStrides(const std::array<size_t, Order>& shape) -> std::array<size_t, Order>
 {
